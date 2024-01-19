dad64be1d4e96379e2ebca063d8fe90e65639a2b
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-19 10:49:27 -0800

    update for 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
+#!/usr/bin/pip
 #Installing flask from pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => package['python3-pip'],
 }
