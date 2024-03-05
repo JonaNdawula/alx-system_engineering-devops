6aba62f97d3aadcbccf903becf7a03a01da1b679
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-03-05 01:19:59 -0800

    script for 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers)"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Will Returns total Number of subscribers on Reddit subreddit"""
+    Reddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    resp = requests.get(Reddit_url, headers={"User-Agent": "app/1.0"})
+    if resp.status_code == 200:
+        data = resp.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
