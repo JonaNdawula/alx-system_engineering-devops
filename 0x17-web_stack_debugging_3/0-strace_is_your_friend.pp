2f04aac0c12a3fd5f6c793cee2018e799568bcf1
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-03-06 12:34:07 -0800

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet script to fix `phpp` extensions to `php` in wordpress file `wp-settings.php`
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settins.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
